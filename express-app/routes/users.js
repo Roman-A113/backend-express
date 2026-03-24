200d2ee615af1c24930e56344d17153c34bacfce
Roman <roman.koles@bk.ru>
2026-03-24 18:53:41 +0500

    fix 5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,27 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 users = [
-    {
-      "id": 1,
-      "name": "Roma"
-    },
-    {
-      "id": 2,
-      "name": "Sonya"
-    },
-    {
-      "id": 3,
-      "name": "Igor"
-    }];
+  {
+    "id": 1,
+    "name": "Roma"
+  },
+  {
+    "id": 2,
+    "name": "Sonya"
+  },
+  {
+    "id": 3,
+    "name": "Igor"
+  }];
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send({
-    items:  users;
+    items: users
   })
 });
 router.post('/', function (req, res, next) {
-  user = {"id": users[users.length - 1] + 1, "name": req.body.name}
+  user = { "id": users[users.length - 1].id + 1, "name": req.body.name }
   users.push(user);
   res.status(201).json(user);
 });
