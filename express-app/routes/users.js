d2b7aaf16466a0b6b9a250426b11cc7814d1cc20
Roman <roman.koles@bk.ru>
2026-03-24 18:38:38 +0500

    done 3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get('/', function (req, res, next) {
+  res.send({
+    items: [
+    {
+      "id": 1,
+      "name": "Roma"
+    },
+    {
+      "id": 2,
+      "name": "Sonya"
+    },
+    {
+      "id": 3,
+      "name": "Igor"
+    }]
+  })
 });
 
 module.exports = router;
